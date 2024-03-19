0fbee112127634e4fb601c1757058cfbfdcd030d
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-03-19 15:46:53 +0100

    add gemm test

--- tests/cocotb/test_streamer_gemm.py
+++ tests/cocotb/test_streamer_gemm.py
@@ -16,6 +16,7 @@
 # ---------------------------------
 
 import cocotb
+import numpy as np
 from cocotb.triggers import RisingEdge, Timer, with_timeout
 from cocotb.clock import Clock
 from cocotb_test.simulator import run
@@ -49,80 +50,154 @@ MAX_WIDE_VAL = 2**WIDE_DATA_WIDTH
 # Configuration found under util/cfg/streamer_cfg.hjson
 # Also some pre-computed that are fixed
 
-CSR_ALU_CONFIG = 0
-CSR_ALU_GPP_1 = 1
-CSR_ALU_GPP_2 = 2
-CSR_ALU_GPP_3 = 3
-CSR_ALU_GPP_4 = 4
-CSR_ALU_GPP_5 = 5
-CSR_ALU_GPP_6 = 6
-CSR_ALU_GPP_7 = 7
+CSR_LOOP_K = 0
+CSR_LOOP_N = 1
+CSR_LOOP_M = 2
 
-# This STREAMER_OFFSET is the offset
-# For the address registers
-STREAMER_OFFSET = 8
+CSR_TEMP_STRIDE_A_0 = 3
+CSR_TEMP_STRIDE_A_1 = 4
+CSR_TEMP_STRIDE_A_2 = 5
+CSR_TEMP_STRIDE_B_0 = 6
+CSR_TEMP_STRIDE_B_1 = 7
+CSR_TEMP_STRIDE_B_2 = 8
+CSR_TEMP_STRIDE_C_0 = 9
+CSR_TEMP_STRIDE_C_1 = 10
+CSR_TEMP_STRIDE_C_2 = 11
 
-CSR_LOOP_COUNT_0 = 0 + STREAMER_OFFSET
-CSR_TEMPORAL_STRIDE_0 = 1 + STREAMER_OFFSET
-CSR_TEMPORAL_STRIDE_1 = 2 + STREAMER_OFFSET
-CSR_TEMPORAL_STRIDE_2 = 3 + STREAMER_OFFSET
-CSR_SPATIAL_STRIDE_0 = 4 + STREAMER_OFFSET
-CSR_SPATIAL_STRIDE_1 = 5 + STREAMER_OFFSET
-CSR_SPATIAL_STRIDE_2 = 6 + STREAMER_OFFSET
-CSR_BASE_PTR_0 = 7 + STREAMER_OFFSET
-CSR_BASE_PTR_1 = 8 + STREAMER_OFFSET
-CSR_BASE_PTR_2 = 9 + STREAMER_OFFSET
-CSR_START_STREAMER = 10 + STREAMER_OFFSET
+CSR_SPAT_STRIDE_A_0 = 12
+CSR_SPAT_STRIDE_A_1 = 13
+CSR_SPAT_STRIDE_B_0 = 14
+CSR_SPAT_STRIDE_B_1 = 15
+CSR_SPAT_STRIDE_C_0 = 16
+CSR_SPAT_STRIDE_C_1 = 17
+
+CSR_BASE_A = 18
+CSR_BASE_B = 19
+CSR_BASE_C = 20
+
+CSR_START_STREAMER = 21
+
+CSR_GEMM_K = 22
+CSR_GEMM_M = 23
+CSR_GEMM_N = 24
+
+CSR_GEMM_SUBS = 25
+CSR_GEMM_START = 26
 
 
 @cocotb.test()
 async def stream_alu_dut(dut):
-    # Value configurations you can set
-    # For exploration and testing
-    # These values go into the respective
-    # CSR register addresses above
-
-    # ACLU_CONFIG has the following:
-    # 0 - addition
-    # 1 - subtraction
-    # 2 - multiplication
-    # 3 - XOR
-    ALU_CONFIG = 1
-    ALU_GPP_1 = 123
-    ALU_GPP_2 = 456
-    ALU_GPP_3 = 789
-    ALU_GPP_4 = 910
-    ALU_GPP_5 = 101
-    ALU_GPP_6 = 121
-    ALU_GPP_7 = 368
-
-    # These ones go into the
-    # streamer registers
-    LOOP_COUNT_0 = 100
-    TEMPORAL_STRIDE_0 = 64
-    TEMPORAL_STRIDE_1 = 64
-    TEMPORAL_STRIDE_2 = 64
-    SPATIAL_STRIDE_0 = 8
-    SPATIAL_STRIDE_1 = 8
-    SPATIAL_STRIDE_2 = 8
-    BASE_PTR_0 = 0
-    BASE_PTR_1 = 32
-    BASE_PTR_2 = 64
 
     # Start clock
     clock = Clock(dut.clk_i, 10, units="ns")
     cocotb.start_soon(clock.start())
+    await snax_util.clock_and_wait(dut)
 
     # Reset dut
     await snax_util.reset_dut(dut)
-
-    # Always active assuming core can
-    # contiuously read data from streamer CSR
 
     # Let simulation time run
     for i in range(10):
         await snax_util.clock_and_wait(dut)
 
+
+    # generate data
+    M = 16
+    N = 16
+    K = 16
+
+    low_bound = 0
+    high_bound = 127
+
+    np.random.seed(0)
+
+    A = np.random.randint(low_bound, high_bound, size=[M, K], dtype=np.dtype("int8"))
+    B = np.random.randint(low_bound, high_bound, size=[K, N], dtype=np.dtype("int8"))
+    # Make sure the product is possible!
+    assert A.shape[1] == B.shape[0]
+    C_golden = np.matmul(A.astype(np.dtype("int32")), B.astype(np.dtype("int32")))
+
+    # transpose B
+    B = np.transpose(B)
+
+    # Flatten the arrays
+    A = A.flatten().tolist()
+    B = B.flatten().tolist()
+    C_golden = C_golden.flatten().tolist()
+
+    # pack everything into a list of 64 bytes = wide tcdm
+    A = snax_util.gen_wide_list(A, 8, 512)
+    B = snax_util.gen_wide_list(B, 8, 512)
+    C_golden = snax_util.gen_wide_list(C_golden, 32, 512)
+
+    inputs = [*A, *B]
+
+    cocotb.log.info("Preload data with DMA control")
+
+    for i in range(len(inputs)):
+        await snax_util.wide_tcdm_write(
+            dut, i * WIDE_BANK_INCREMENT, inputs[i]
+        )
+
+    await snax_util.wide_tcdm_clr(dut)
+
+    cocotb.log.info("Setting up of CSR registers")
+
+    K_param = round(K//8)
+    N_param = round(N//8)
+    M_param = round(M//8)
+
+    
+    await snax_util.reg_write(dut, CSR_LOOP_K, K_param)
+    await snax_util.reg_write(dut, CSR_LOOP_N, N_param)
+    await snax_util.reg_write(dut, CSR_LOOP_M, M_param)
+
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_A_0, 8)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_A_1, 0)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_A_2, 64 * K_param)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_B_0, 8)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_B_1, 64 * K_param)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_B_2, 0)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_C_0, 0)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_C_1, 32)
+    await snax_util.reg_write(dut, CSR_TEMP_STRIDE_C_2, 256 * N_param)
+
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_A_0, 1)
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_A_1, 16)
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_B_0, 1)
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_B_1, 16)
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_C_0, 4)
+    await snax_util.reg_write(dut, CSR_SPAT_STRIDE_C_1, 64)
+
+    A_offset = 0
+    await snax_util.reg_write(dut, CSR_BASE_A, A_offset)
+    B_offset = A_offset + 64 * K_param * M_param
+    await snax_util.reg_write(dut, CSR_BASE_B, B_offset)
+    C_offset = B_offset + 64 * N_param * K_param
+    await snax_util.reg_write(dut, CSR_BASE_C, C_offset)
+
+    await snax_util.reg_write(dut, CSR_START_STREAMER, 1)
+
+    await snax_util.reg_write(dut, CSR_GEMM_K, K_param)
+    await snax_util.reg_write(dut, CSR_GEMM_M, M_param)
+    await snax_util.reg_write(dut, CSR_GEMM_N, N_param)
+
+    await snax_util.reg_write(dut, CSR_GEMM_SUBS, 0)
+    await snax_util.reg_write(dut, CSR_GEMM_START, 1)
+
+    await snax_util.reg_clr(dut)
+
+    # wait for finish
+    await snax_util.reg_write(dut, CSR_GEMM_START, 0)
+    await snax_util.reg_write(dut, CSR_START_STREAMER, 0)
+    cocotb.log.info("GEMM Operation Finished")
+    await snax_util.reg_clr(dut)
+
+
+    # Read the result and check
+    for i in range(len(C_golden)):
+        tcdm_wide_val = await snax_util.wide_tcdm_read(dut, C_offset + i * WIDE_BANK_INCREMENT)
+        snax_util.comp_and_assert(C_golden[i], tcdm_wide_val)
 
 # Main test run
 def test_streamer_gemm(simulator, waves):
